21ab2e4c2da05d023e207974f6683b0f9e7d2202
gloryakinyi <akinyiglory22gmail.com>
2024-04-29 12:17:31 +0300

    initial commit

--- app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/home/HomeScreen.kt
@@ -1,61 +1,44 @@
 package com.example.wazitoecommerce.ui.theme.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.wazitoecommerce.navigation.ADD_PRODUCTS_URL
-import com.example.wazitoecommerce.navigation.HOME_URL
-import com.example.wazitoecommerce.navigation.LOGIN_URL
-import com.example.wazitoecommerce.navigation.VIEW_PRODUCTS_URL
-import com.example.wazitoecommerce.ui.theme.NewBlue
 import com.example.wazitoecommerce.ui.theme.WazitoECommerceTheme
 
-
-data class Screen(val title: String, val icon: Int)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,77 +51,65 @@ fun HomeScreen(navController:NavHostController){
         var selected by remember { mutableIntStateOf(0) }
         Scaffold(
             bottomBar = {
-                        NavigationBar {
-                            bottomNavItems.forEachIndexed { index, bottomNavItem ->
-                                NavigationBarItem(
-                                    selected = index == selected,
-                                    onClick = {
-                                        selected = index
-                                        // navController.navigate(HOME_URL)
-                                    },
-                                    icon = {
-                                        BadgedBox(
-                                            badge = {
-                                                if (bottomNavItem.badges != 0) {
-                                                    Badge {
-                                                        Text(text = bottomNavItem.badges.toString())
-                                                    }
-                                                } else if (bottomNavItem.hasNews) {
-                                                    Badge()
-                                                }
+                NavigationBar {
+                    bottomNavItems.forEachIndexed { index, bottomNavItem ->
+                        NavigationBarItem(
+                            selected = index == selected,
+                            onClick = {
+                                selected = index
+                                navController.navigate(bottomNavItem.route)
+                            },
+                            icon = {
+                                BadgedBox(
+                                    badge = {
+                                        if (bottomNavItem.badges != 0) {
+                                            Badge {
+                                                Text(text = bottomNavItem.badges.toString())
                                             }
-                                        ) {
-                                            Icon(imageVector =
-                                                if (index == selected)
-                                                    bottomNavItem.selectedIcon
-                                                else
-                                                    bottomNavItem.unselectedIcon,
-                                                contentDescription = bottomNavItem.title)
+                                        } else if (bottomNavItem.hasNews) {
+                                            Badge()
                                         }
+                                    }
+                                ) {
+                                    Icon(imageVector =
+                                    if (index == selected)
+                                        bottomNavItem.selectedIcon
+                                    else
+                                        bottomNavItem.unselectedIcon,
+                                        contentDescription = bottomNavItem.title)
+                                }
 
-                                    },
-                                    label = {
-                                        Text(text = bottomNavItem.title)
-                                    })
-                            }
-                        }
+                            },
+                            label = {
+                                Text(text = bottomNavItem.title)
+                            })
+                    }
+                }
             },
 
 
             floatingActionButton = {
-                                   FloatingActionButton(onClick = { /*TODO*/ }) {
-                                       IconButton(onClick = { /*TODO*/ }) {
-                                           Icon(imageVector = Icons.Default.Add,
-                                               contentDescription = "menu")
-                                       }
-                                   }
-            },
-
-
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Homepage")},
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(NewBlue),
-                    navigationIcon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.Menu,
-                                contentDescription = "menu")
-                        }
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Add,
+                            contentDescription = "menu")
                     }
-                )
+                }
+            },
+            //Content Section
+            content = @Composable{
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "Homepage")
+
+                }
+
             }
-        ) {
 
-
-            Column {
-                Text(
-                    text = "Homepage",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-
-        }
+        )
 
     }
 }
@@ -153,16 +124,18 @@ val bottomNavItems = listOf(
         unselectedIcon=Icons.Outlined.Home,
         hasNews = false,
         badges=0
-        ),
+    ),
+
+
 
     BottomNavItem(
-            title = "Login",
-            route="login",
-            selectedIcon=Icons.Filled.Person,
-            unselectedIcon=Icons.Outlined.Person,
-            hasNews = true,
-            badges=5
-         ),
+        title = "Login",
+        route="login",
+        selectedIcon=Icons.Filled.Person,
+        unselectedIcon=Icons.Outlined.Person,
+        hasNews = true,
+        badges=5
+    ),
 
     BottomNavItem(
         title = "Signup",
@@ -174,7 +147,7 @@ val bottomNavItems = listOf(
     ),
 
 
-)
+    )
 
 
 
